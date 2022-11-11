3c97ac6a7c85ff8a42b300dbd92fd039862edd51
Ashutosh Swain <ashutosh.ashain@gmail.com>
2022-11-11 06:36:19 -0800

    reddis inclusion

--- Inventory/main.py
+++ Inventory/main.py
@@ -3,6 +3,13 @@ from redis_om import get_redis_connection
 
 app = FastAPI()
 
+redis = get_redis_connection(
+    host="redis-14081.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+    port=14081,
+    password="NpX1njzFhOgKBiUdHOUSuwvjouVvKAtX",
+    
+    
+)
 
 @app.get("/")
 async def root():
