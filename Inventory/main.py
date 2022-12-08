53b7dcfd8b8ef5fc682119d7de7d413b8119a414
Ashutosh Swain <ashutosh.ashain@gmail.com>
2022-12-08 21:41:17 +0530

    edit

--- Inventory/main.py
+++ Inventory/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from redis_om import get_redis_connection
+from fastapi.middleware.cors import CORSMiddleware
 from redis_om import HashModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://127.0.0.1:8000'],
+    allow_methods=['*'],
+    
+)
 
 redis = get_redis_connection(
     host="redis-14081.c301.ap-south-1-1.ec2.cloud.redislabs.com",
@@ -21,9 +29,9 @@ class Product(HashModel):
     class Meta:
         database = redis
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get('/products')
+def all():
+    return Product.all_pks()
 
 
 
