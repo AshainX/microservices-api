0c3f3a39eda1d1c713cd8575875250a93490cf9a
Ashutosh Swain <ashutosh.ashain@gmail.com>
2022-12-08 20:05:57 +0530

    hashmodel

--- Inventory/main.py
+++ Inventory/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from redis_om import get_redis_connection
+from redis_om import HashModel
 
 app = FastAPI()
 
@@ -7,9 +8,18 @@ redis = get_redis_connection(
     host="redis-14081.c301.ap-south-1-1.ec2.cloud.redislabs.com",
     port=14081,
     password="NpX1njzFhOgKBiUdHOUSuwvjouVvKAtX",
+    decode_response=True
     
     
 )
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis
 
 @app.get("/")
 async def root():
